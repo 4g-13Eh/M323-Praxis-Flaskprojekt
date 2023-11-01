8fe585b0b083aebc0911eb2b83275475bcdc615b
Shahin Amon <amons@bzz.ch>
2023-11-01 15:39:24 +0100

    initialise project

---
+++ main.py
@@ -0,0 +1,4 @@
+from app import app  # Hier importierst du die Flask-App aus deinem Anwendungsmodul
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Starte die Flask-Anwendung im Debug-Modus
