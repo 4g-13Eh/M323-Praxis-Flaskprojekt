5e27340be6683f8cdc7984ac5ecc21d8329a14b8
Shahin Amon <amons@bzz.ch>
2023-11-12 11:59:17 +0100

    errorhandling

--- main.py
+++ main.py
@@ -20,33 +20,36 @@ def mul(a, b):
 
 @app.route('/a1g', methods=['POST'])
 def endpoint_a1g():
-    data = request.get_json()
-    operation = data['operation']
-    num1 = data['num1']
-    num2 = data['num2']
-    match operation:
-        case 'add':
-            sum = add(num1, num2)
-        case 'sub':
-            sum = sub(num1, num2)
-        case 'mul':
-            sum = mul(num1, num2)
-    return jsonify({"result": sum}), 200
+    try:
+        data = request.get_json()
+        operation = data['operation']
+        num1 = data['num1']
+        num2 = data['num2']
+        match operation:
+            case 'add':
+                sum = add(num1, num2)
+            case 'sub':
+                sum = sub(num1, num2)
+            case 'mul':
+                sum = mul(num1, num2)
+        return jsonify({"result": sum}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 #A1F
 @app.route('/a1f', methods=['POST'])
 def endpoint_a1f():
+    try:
         data = request.get_json()
-        if data:
-            result_tuple = tuple(data) # Liste in Tupel umwandeln
-            try:
-                result_tuple[0] = 'a' # Versuch, das erste Element zu ändern
-                message = "Tupel veraendert."
-            except TypeError as e:
-                message = "Tupel nicht verändert. Ist nicht möglich 💀"
-            return jsonify({"result_tuple": result_tuple, "message": message}), 200
-        else:
-            return jsonify({"error": "Ungueltige Daten. Bitte senden Sie eine Liste."}), 400
+        result_tuple = tuple(data) # Liste in Tupel umwandeln
+        try:
+            result_tuple[0] = 'a' # Versuch, das erste Element zu ändern
+            message = "Tupel veraendert."
+        except TypeError as e:
+            message = "Tupel nicht verändert. Ist nicht möglich 💀"
+        return jsonify({"result_tuple": result_tuple, "message": message}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
     
 #A1E
 #OO
@@ -72,18 +75,21 @@ def measure_execution_time(func, *args):
 
 @app.route('/a1e', methods=['POST'])
 def endpoint_a1e():
-    data = request.get_json()
-    calc = SquareCalculator()
-    oo_res, oo_time = measure_execution_time(calc.calculate_square, data)
-    proc_res, proc_time = measure_execution_time(calculate_square, data)
-    func_res, func_time = measure_execution_time(calculate_square, data)
-    # Sortierung und Formatierung der Ergebnisse für den Vergleich
-    results = [
-        {"type": "Object Oriented", "result": oo_res, "executiontime": oo_time},
-        {"type": "Procedural", "result": proc_res, "executiontime": proc_time},
-        {"type": "Functional", "result": func_res, "executiontime": func_time}
-    ]
-    return jsonify({"sorted_results": results}), 200
+    try:
+        data = request.get_json()
+        calc = SquareCalculator()
+        oo_res, oo_time = measure_execution_time(calc.calculate_square, data)
+        proc_res, proc_time = measure_execution_time(calculate_square, data)
+        func_res, func_time = measure_execution_time(calculate_square, data)
+        # Sortierung und Formatierung der Ergebnisse für den Vergleich
+        results = [
+            {"type": "Object Oriented", "result": oo_res, "executiontime": oo_time},
+            {"type": "Procedural", "result": proc_res, "executiontime": proc_time},
+            {"type": "Functional", "result": func_res, "executiontime": func_time}
+        ]
+        return jsonify({"sorted_results": results}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 #B1G
@@ -99,11 +105,14 @@ def gcd(a, b):
 
 @app.route('/b1g', methods=['POST'])
 def endpoint_b1g():
-    data = request.get_json()
-    a = data['a']
-    b = data['b']
-    result = gcd(a, b)
-    return jsonify({"result": result}), 200
+    try:
+        data = request.get_json()
+        a = data['a']
+        b = data['b']
+        result = gcd(a, b)
+        return jsonify({"result": result}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 #B1F
@@ -120,10 +129,13 @@ def fibonacci(n):
 
 @app.route('/b1f', methods=['POST'])
 def endpoint_b1f():
-    data = request.get_json()
-    n = data['n']
-    result = fibonacci(n)
-    return jsonify({"result": result}), 200
+    try:
+        data = request.get_json()
+        n = data['n']
+        result = fibonacci(n)
+        return jsonify({"result": result}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 #B1E
 def bubble_sort(arr):
@@ -146,22 +158,25 @@ def format_nums(numbers):
 
 @app.route('/b1e', methods=['POST'])
 def endpoint_b1e():
+    try:
         data = request.get_json()
-        if data:
-            bubble_sort(data)
-            formatted_nums = format_nums(data)
-            return jsonify({"result": data, "formatted_result": formatted_nums}), 200
-        else:
-            return jsonify({"error": "Ungueltige Daten. Bitte senden Sie eine Liste"}), 400
+        bubble_sort(data)
+        formatted_nums = format_nums(data)
+        return jsonify({"result": data, "formatted_result": formatted_nums}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
     
 #B2G
 @app.route('/b2g', methods=['POST'])
 def endpoint_b2g():
-    data = request.get_json()
-    a = data['a']
-    b = data['b']
-    sum = add(a, b)
-    return jsonify({"result": sum}), 200
+    try:
+        data = request.get_json()
+        a = data['a']
+        b = data['b']
+        sum = add(a, b)
+        return jsonify({"result": sum}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 #B2F
 def power_of(func, a, b):
@@ -180,20 +195,15 @@ def endpoint_b2f():
         operation = data['operation']
         num1 = data['num1']
         num2 = data['num2']
-        if num1 and num2:
-            num1 = int(num1)
-            num2 = int(num2)
-            match operation:
-                case 'add':
-                    operation = add
-                case 'sub':
-                    operation = sub
-                case 'mul':
-                    operation = mul
-            result = power_of(operation, num1, num2)
-            return jsonify({"result": result}), 200
-        else:
-            return jsonify({"error": "Ungueltige Daten. Bitte senden Sie eine Liste."}), 400
+        match operation:
+            case 'add':
+                operation = add
+            case 'sub':
+                operation = sub
+            case 'mul':
+                operation = mul
+        result = power_of(operation, num1, num2)
+        return jsonify({"result": result}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -209,25 +219,21 @@ def endpoint_b2e():
         data = request.get_json()
         text = data['text']
         name = data['name']
-        if text and name:
-            text = greet(text)
-            result = text(name)
-            return jsonify({"result": result}), 200
+        text = greet(text)
+        result = text(name)
+        return jsonify({"result": result}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
 #B3G
 @app.route('/b3g', methods=['POST'])
 def endpoint_b3g():
-        try:
-            data = request.get_json()
-            if data:
-                square = lambda data: data ** 2
-                return jsonify({"result": square(int(data))}), 200
-            else:
-                return jsonify({"error": "Ungueltige Daten. Bitte senden Sie eine Liste."}), 400
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
+    try:
+        data = request.get_json()
+        square = lambda data: data ** 2
+        return jsonify({"result": square(int(data))}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 #B3F
 @app.route('/b3f', methods=['POST'])
@@ -236,11 +242,8 @@ def endpoint_b3f():
         data = request.get_json()
         list1 = data['list1']
         list2 = data['list2']
-        if list1 and list2:
-            result = list(map(lambda x, y: max(x, y), list1, list2))
-            return jsonify({"result": result}), 200
-        else:
-            return jsonify({"error": "Ungueltige Daten. Bitte senden Sie eine Liste."}), 400
+        result = list(map(lambda x, y: max(x, y), list1, list2))
+        return jsonify({"result": result}), 200    
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -251,15 +254,12 @@ def endpoint_b3e():
         data = request.get_json()
         list = data['list']
         sort = data['sort']
-        if list:
-            match sort:
-                case 'asc':
-                    list = sorted(list, key=lambda x: len(x))
-                case 'desc':
-                    list = sorted(list, key=lambda x: len(x), reverse=True)
-            return jsonify({"result": list}), 200
-        else:
-            return jsonify({"error": "Ungueltige Daten. Bitte senden Sie eine Liste."}), 400
+        match sort:
+            case 'asc':
+                list = sorted(list, key=lambda x: len(x))
+            case 'desc':
+                list = sorted(list, key=lambda x: len(x), reverse=True)
+        return jsonify({"result": list}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -270,31 +270,30 @@ def endpoint_b4g():
         data = request.get_json()
         operation = data['operation']
         input_list = data['list']
-        if data:
-            match operation:
-                case 'map':
-                    result = list(map(lambda x: x * x, input_list))
-                case 'filter':
-                    result = list(filter(lambda x: x % 2 == 0, input_list))
-                case 'reduce':
-                    input_list = list(map(int, input_list))
-                    result = reduce(lambda x, y: x + y, input_list)
-            return jsonify({"result": result}), 200
-        else:
-            return jsonify({"error": "Ungueltige Daten. Bitte senden Sie eine Liste."}), 400
+        match operation:
+            case 'map':
+                result = list(map(lambda x: x * x, input_list))
+            case 'filter':
+                result = list(filter(lambda x: x % 2 == 0, input_list))
+            case 'reduce':
+                input_list = list(map(int, input_list))
+                result = reduce(lambda x, y: x + y, input_list)
+        return jsonify({"result": result}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
 
 #B4F
 @app.route('/b4f', methods=['POST'])
 def endpoint_b4f():
-    data = request.get_json()
-    # Potenziert jede Zahl in der Liste mit 3
-    # Filtert alle ungeraden Zahlen aus der Liste
-    # Addiert alle Zahlen in der übrigen Liste zusammen
-    result = reduce(lambda x, y: x + y, filter(lambda x: x % 2 == 0, map(lambda x: x ** 3, data)))
-    return jsonify({"result": result}), 200
+    try:
+        data = request.get_json()
+        # Potenziert jede Zahl in der Liste mit 3
+        # Filtert alle ungeraden Zahlen aus der Liste
+        # Addiert alle Zahlen in der übrigen Liste zusammen
+        result = reduce(lambda x, y: x + y, filter(lambda x: x % 2 == 0, map(lambda x: x ** 3, data)))
+        return jsonify({"result": result}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 #B4E
 def get_average_population(data):
@@ -310,11 +309,14 @@ def rearange_data(data):
 
 @app.route('/b4e', methods=['POST'])
 def endpoint_b4e():
-    data = request.get_json()
-    average_population = get_average_population(data)
-    rearanged_data = rearange_data(data)
-    return jsonify({"average_population_asia_10": average_population, 
-                    "rearanged_data": rearanged_data}), 200
+    try:
+        data = request.get_json()
+        average_population = get_average_population(data)
+        rearanged_data = rearange_data(data)
+        return jsonify({"average_population_asia_10": average_population, 
+                        "rearanged_data": rearanged_data}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
     
 
 if __name__ == '__main__':
